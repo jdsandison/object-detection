43c1e11cd297ae909b3d8b931b0afe0abb8717b9
Jack Sandison <jacksandison11@gmail.com>
2024-05-29 15:17:16 +0100

    organising data and planning
    Organising the images into training and testing sets. and planning the structure of the code.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import pandas as pd
 import numpy as np
+
+# load models
+
+# load image
+
+# detect license plate
+
+# crop license plate
+
+# process license plate
+
+# read license plate
+
+# print results
